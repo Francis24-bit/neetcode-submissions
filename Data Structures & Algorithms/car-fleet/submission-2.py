f83d084b0399004d61e664e98d2796566c0bd1d0
Francis <zhou.erli30@gmail.com>
2026-09-08 16:12:59 -0700

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # this problem asks how many fleet can arrive at the destination at the same time(as when you catch up, you can only travel as fast as the car before you)
+
+        # in other words, a fleet in code is represented as the time at which one car or several cars arrive at the destination.
+
+        # so we start with the nearest car to the destinaiton(zip the speed and position, and sort), figure out its time to arrive, and look at the arrive time of the car behind it.
+
+        # if the car behind travels faster, it can only arrive at the target with the car before it(per rule), in this case, they are in one group.
+
+        # the key is to understand that different fleets are actually different time that cars arrive.
+        # stack = []
+        # for car in cars:
+            # time = (target - car[0]) / car[1]
+            # if not stack or time > stack[-1]:
+                # stack.append(time)
+
+        cars = list(zip(position, speed))
+        cars.sort(reverse = True)
+
+        count = 1
+        current_time = (target - cars[0][0]) / cars[0][1]
+
+        for i in range(1, len(cars)):
+            new_time = (target - cars[i][0]) / cars[i][1]
+        
+            if new_time > current_time:
+                count += 1
+                current_time = new_time
+        
+        return count
