68b9ab544923d11fbfd536687ba98b07e4209a0a
Francis <zhou.erli30@gmail.com>
2026-09-09 12:16:12 -0700

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        left = 0
+        right = m * n - 1
+
+# left/right is the range, meaning you need to change the index. but when compare, you need to comapre the value
+
+# so find the index, then find the number, compare the number, but move the index
+        while left <= right:
+            mid_index = left + (right - left) // 2
+            row = mid_index // n
+            column = mid_index % n
+            mid_number = matrix[row][column]
+
+            if mid_number < target:
+                left = mid_index + 1
+            elif mid_number > target:
+                right = mid_index - 1
+            else:
+                return True
+        
+        return False
